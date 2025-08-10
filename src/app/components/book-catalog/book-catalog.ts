a501e71109bf759c60641ee2d08e301b50892271
TatsHats <6642017@gmail.com>
2025-08-10 21:36:00 +0300

    feat: add a list of books to the table

--- src/app/components/book-catalog/book-catalog.ts
+++ src/app/components/book-catalog/book-catalog.ts
@@ -1,13 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Book } from '../../models/book.interface';
+import { HttpClient } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-book-catalog',
-  imports: [],
+  standalone: true,
+  imports: [MatTableModule, AsyncPipe],
   templateUrl: './book-catalog.html',
   styleUrl: './book-catalog.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BookCatalog {
+export class BookCatalog implements OnInit {
 
+  books$!: Observable<Book[]>;
+  columnsToDisplay = ['title', 'author', 'year'];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.books$ = this.http.get<Book[]>('/books.json');
+  }
 }
